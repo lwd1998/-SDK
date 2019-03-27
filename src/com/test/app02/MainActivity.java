33ea030946d6856e7a192d5e447c1cf82243ba95
liweide <1980933521@qq.com>
2019-03-27 11:04:08 +0800

    调用百度地图SDK实现地图功能

---
+++ src/com/test/app02/MainActivity.java
@@ -0,0 +1,41 @@
+package com.test.app02;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	private MapView mMapView ;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+	private void initView() {
+		mMapView = (MapView) findViewById(R.id.id_bmapView);
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
+        mMapView.onResume();  
+	}
+@Override
+	protected void onPause() {
+		super.onPause();
+		//在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
+        mMapView.onPause();  
+	}
+@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		//在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
+        mMapView.onDestroy();  
+	}
+}
